f9f4aa3ab81aaf1af715777fd1623647e16edf40
Arthur Leung <1808arthur@gmail.com>
2026-02-14 15:00:35 -0400

    Updated W04

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,5 +1,10 @@
 const isAuthenticated = (req, res, next) => {
-    if (!req.isAuthenticated()) {
+    console.log("--- Auth Check Triggered ---");
+    console.log("Session User Data:", req.session.user);
+    
+    // return res.status(401).json("Auth Middleware is working!"); 
+
+    if (req.session.user === undefined) {
         return res.status(401).json("You do not have access.");
     }
     next();
